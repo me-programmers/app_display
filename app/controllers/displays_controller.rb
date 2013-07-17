f089b8aba7a747b08870242346bbd4438a546313
me-programmers <me.programmers@gmail.com>
2013-07-17 12:00:09 +0800

    update order by

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -26,11 +26,10 @@ class DisplaysController < ApplicationController
 		@geteceranpg2 = MarketComparison.select("komoditi, eceran, jenis").where("eceran > 0 and jenis ='Sayuran'").limit(4).offset(4)
 		@geteceranpg3 = MarketComparison.select("komoditi, eceran, jenis").where("eceran > 0 and jenis ='Sayuran'").limit(4).offset(8)
 		@geteceranpg4 = MarketComparison.select("komoditi, eceran, jenis").where("eceran > 0 and jenis ='Beras'").limit(4)
-		@geteceranpg5 = MarketComparison.select("komoditi, eceran, jenis").where("eceran > 0 and jenis ='Holtikultura'").limit(5)
+		@geteceranpg5 = MarketComparison.select("komoditi, eceran, jenis").where("eceran > 0 and jenis ='Palawija'").limit(5)
 		@getgrosir1pg1 = MarketComparison.select("komoditi, grosir1, jenis").where("grosir1 > 0 and jenis='Sayuran'").limit(4)
 		@getgrosir1pg2 = MarketComparison.select("komoditi, grosir1, jenis").where("grosir1 > 0 and jenis='Sayuran'").limit(4).offset(4)
 		@getgrosir1pg3 = MarketComparison.select("komoditi, grosir1, jenis").where("grosir1 > 0 and jenis='Sayuran'").limit(4).offset(8)
-		@getgrosir2pg4 = MarketComparison.select("komoditi, grosir2, jenis").where("grosir2 > 0 and jenis='Holtikultura'").limit(5)
-		
+		@getgrosir2pg4 = MarketComparison.select("komoditi, grosir2, jenis").where("grosir2 > 0 and jenis='Palawija'").limit(5)		
 	end
 end
--- app/models/market_comparison.rb
+++ app/models/market_comparison.rb
@@ -1,3 +1,4 @@
 class MarketComparison < ActiveRecord::Base
+  default_scope order('id ASC')
   attr_accessible :eceran, :grosir1, :grosir2, :grosir3, :grosir4, :grosir5, :komoditi, :marketbg, :marketloc, :petani, :jenis
 end
